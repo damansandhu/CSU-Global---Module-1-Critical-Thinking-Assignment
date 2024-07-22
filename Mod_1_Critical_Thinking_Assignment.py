e014dc574f3c601c8ff6b79e4e2bcb495d3c59ff
Damanbir Sandhu <damanpsandhu@gmail.com>
2024-07-21 19:45:52 -0700

    Add files via upload

---
+++ Mod_1_Critical_Thinking_Assignment.py
@@ -0,0 +1,54 @@
+# Creating Python Programs
+
+# Part 1:
+
+# Write a Python program to find the addition and subtraction of two numbers.
+# Ask the user to input two numbers (num1 and num2). Given those two numbers, 
+# add them together to find the output. Also, subtract the two numbers to find the output.
+
+# Addition
+print("Please input two numbers to add together:")
+print("First input:")
+num1 = int(input())
+
+print("Second input:")
+num2 = int(input())
+print(num1, '+', num2, '=', num1 + num2)
+
+# Subtraction
+print("Please input two numbers to subtract:")
+print("First input:")
+num1 = int(input())
+
+print("Second input:")
+num2 = int(input())
+print(num1, '-', num2, '=', num1 - num2)
+
+# Part 2:
+
+# Write a Python program to find the multiplication and division of two numbers.
+# Ask the user to input two numbers (num1 and num2). Given those two numbers, multiply 
+# them together to find the output. Also, divide num1/num2 to find the output.
+
+# Multiplication
+print("Please input two numbers to multiply together:")
+print("First input:")
+num1 = int(input())
+
+print("Second input:")
+num2 = int(input())
+print(num1, '*', num2, '=', num1 * num2)
+
+# Division
+print("Please input two numbers to divide:")
+print("First input:")
+num1 = int(input())
+
+print("Second input:")
+num2 = int(input())
+print(num1, '/', num2, '=', num1 / num2)
+
+
+# Compile and submit your pseudocode, source code, and screenshots of the
+# application executing the code from parts 1 and 2, the results and GIT repository in a
+# single document (Word is preferred).
